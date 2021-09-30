2de892482c85fb94d79bfb5ff92534fe56592871
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-09-30 16:26:55 +0530

    Added TabView customization sample

---
+++ TabViewCustomizationSample/TabViewCustomizationSample/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace TabViewCustomizationSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+    }
+}
---
+++ TabViewCustomizationSample/TabViewCustomizationSample/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace TabViewCustomizationSample
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            return builder.Build();
+        }
+    }
+}
