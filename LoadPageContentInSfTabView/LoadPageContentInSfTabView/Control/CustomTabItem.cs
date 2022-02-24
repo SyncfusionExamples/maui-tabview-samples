d2bac6a63f26c23a7470fbce50dfcf8a82db4a2b
Kumaraguru1081 <62003632+Kumaraguru1081@users.noreply.github.com>
2022-02-24 18:06:53 +0530

    Updated the review changes.

---
+++ LoadPageContentInSfTabView/LoadPageContentInSfTabView/Control/CustomTabItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using Syncfusion.Maui.TabView;
+
+namespace LoadPageContentInSfTabView.Control
+{
+    public class CustomTabItem : SfTabItem
+    {
+        /// <summary>
+        /// Identifies the <see cref="Page"/> bindable property.
+        /// </summary>
+        /// <value>
+        /// The identifier for <see cref="Page"/> bindable property.
+        /// </value>
+        public static readonly BindableProperty PageProperty =
+            BindableProperty.Create(nameof(Page), typeof(ContentPage), typeof(CustomTabItem), null, propertyChanged: OnPagePropertyChanged);
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomTabItem"/> class.
+        /// </summary>
+        public CustomTabItem()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the content page.
+        /// </summary>
+        /// <value>
+        /// Specifies the content page of the custom tab item. The default value is null.
+        /// </value>
+        public ContentPage Page
+        {
+            get => (ContentPage)GetValue(PageProperty);
+            set => SetValue(PageProperty, value);
+        }
+
+        /// <summary>
+        /// Invoked when the <see cref="PageProperty"/> is set for the control.
+        /// </summary>
+        /// <param name="bindable">The bindable.</param>
+        /// <param name="oldValue">The old value.</param>
+        /// <param name="newValue">The new value.</param>
+        private static void OnPagePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var page = bindable as CustomTabItem;
+
+            if (page != null)
+            {
+                page.Content = (newValue as ContentPage).Content;
+            }
+        }
+    }
+}
