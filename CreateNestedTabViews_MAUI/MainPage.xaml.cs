971fb744af47dffc6265ca59fe68771a2470519a
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2023-05-30 17:32:52 +0530

    Added nested tab view sample for video

---
+++ CreateNestedTabViews_MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CreateNestedTabViews_MAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ CreateNestedTabViews_MAUI/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateNestedTabViews_MAUI
+{
+	public class ContactInfo
+	{
+		public string Name { get; set; }
+		public long Number { get; set; }
+	}
+
+	public class Person
+	{
+		public string Name { get; set; }
+		public double Height { get; set; }
+	}
+
+	public class Product
+	{
+		public string Name { get; set; }
+		public double SalesRate { get; set; }
+	}
+}
