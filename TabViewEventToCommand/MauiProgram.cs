ce0ea4ed83bf3f44d3b94ef5d15db3ec453c1e48
Brundha <brundha.sf3969@gmail.com>
2024-07-18 18:08:45 +0530

    Added the sample for TabView EventToCommand behaviour.

---
+++ TabViewEventToCommand/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace TabViewEventToCommand
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureSyncfusionCore()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ TabViewEventToCommand/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using Syncfusion.Maui.TabView;
+namespace TabViewEventToCommand
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private Command<object>? selectionChangedCommand;
+        public Command<object>? SelectionChangedCommand
+        {
+            get { return selectionChangedCommand; }
+            set
+            {
+                selectionChangedCommand = value;
+                OnPropertyChanged(nameof(SelectionChangedCommand));
+            }
+        }
+        public MainPageViewModel() 
+        {
+            SelectionChangedCommand = new Command<object>(TabViewSelectionChanged);
+        }
+
+        public void TabViewSelectionChanged(object obj)
+        {
+            // Do action
+
+            var tabView = (obj as SfTabView);
+            if (tabView != null)
+            {
+                foreach(var tabItem  in tabView.Items)
+                {
+                    if (tabItem != null)
+                    {
+                        if(tabItem.IsSelected)
+                        {
+                            App.Current?.MainPage?
+                                .DisplayAlert("Alert", "You have been selected " + tabItem.Header + " Tab", "OK");
+                        }
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
