44d4b581f59ef108099cd636b7eaea71bf890689
Brundha <brundha.sf3969@gmail.com>
2024-07-18 16:38:56 +0530

    Added sample for TabView Selected Header Color Customization

---
+++ SelectedHeaderColorCustomization/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SelectedHeaderColorCustomization
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SelectedHeaderColorCustomization/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SelectedHeaderColorCustomization
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
